831218680472f62d810e8408ada54abbdcfc0c06
Nicholas <nicksteinmetz2@gmail.com>
2017-01-20 11:29:18 -0500

    Create the handler

--- playerhandler.lua
+++ playerhandler.lua
@@ -1,1 +1,19 @@
+local PHandler = {}
 
+function PHandler.IteratePlayers()
+  local p = game.Players:GetPlayers()
+  for i = 1, #p do
+    return p[i]
+  end
+end
+function PHandler.KickPlayer(p, reason)
+  game.Players[p]:Kick(reason)
+end
+function PHandler.Greet(title, text)
+  game.StarterGui:SetCore("SendNotification", {
+       Title = title,
+       Text = text
+   })
+end
+
+return PHandler

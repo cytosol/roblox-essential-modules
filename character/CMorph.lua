1f5153f74ca018bf06769bc2768d28f4b0849321
Nicholas <nicksteinmetz2@gmail.com>
2017-01-20 11:17:36 -0500

    Updated, added initial code

--- character/CMorph.lua
+++ character/CMorph.lua
@@ -1,1 +1,31 @@
+-- Character Morphs
+local CharModule = {}
 
+function GiveHat(char, id)
+  local Insert = game:GetService("InsertService")
+  if char and id then
+    local hat = Insert:LoadAsset(id)
+    local a = hat:GetChildren()[1]
+    a.Parent = char
+  end
+end
+function GiveClothing(char, shirtid, pantsid)
+  if char then
+    local stemp = char["Shirt"]
+    local ptemp = char["Pants"]
+    if shirtid and pantsid or shirtid or pantsid then
+      if stemp ~= nil then stemp.ShirtTemplate = "rbxassetid://"..id
+        else
+      local a = Instance.new("ShirtTemplate")
+      a.ShirtTemplate = "rbxassetid://"..id
+      end
+      if ptemp ~= nil then ptemp.PantsTemplate = "rbxassetid://"..id
+        else
+      local a = Instance.new("PantsTemplate")
+      a.PantsTemplate = "rbxassetid://"..id
+      end
+    end
+  end
+end
+
+return CharModule
